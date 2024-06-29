85fc65ff4f784ace781189b6e434691288fd40f2
choudharymahi74 <choudharymahi565@gmail.com>
2024-06-29 09:04:59 +0530

    Initial commit

---
+++ fullstack-intern-assignment/redux/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import { TypedUseSelectorHook } from "react-redux";
+import authReducer from "./auth/auth.slice";
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch<AppDispatch>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default store;
